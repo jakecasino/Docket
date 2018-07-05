6f5cdd76f3d42ada27a29b5542a0aae324a0f362
Jake Casino <jake@jakecasino.com>
2018-07-04 20:06:12 -0700

    Initial upload to GitHub

---
+++ Docket/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Docket
+//
+//  Created by Jake Casino on 12/13/17.
+//  Copyright © 2017 Jake Casino, Inc. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class ViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
