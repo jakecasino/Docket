754854b69aa6b22f0c3ceac88626f4d757d48b2a
Jake Casino <jake@jakecasino.com>
2018-07-19 11:57:46 -0700

    Initialized the Docket Assistant Model (Machine Learning)

---
+++ Docket/Assistant.swift
@@ -0,0 +1,36 @@
+//
+//  Machine Learning Algorithm.swift
+//  Docket
+//
+//  Created by Jake Casino on 7/19/18.
+//  Copyright © 2018 Jake Casino, Inc. All rights reserved.
+//
+
+import Foundation
+
+let Assistant = DocketAssistantModel()
+
+class DocketAssistantModel {
+	var recommendedHoursOfSleep: Double?
+	
+	fileprivate init() {
+		
+	}
+	
+	func estimateTimeForBreakfast() {
+		
+	}
+	
+	func estimateTimeForLunch() {
+		
+	}
+	
+	func estimateTimeForDinner() {
+		
+	}
+	
+	func estimateTimeForSleep() {
+		
+	}
+	
+}
--- Docket/MenuBar.swift
+++ Docket/MenuBar.swift
@@ -12,5 +12,6 @@ class DKTMenuBarViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+		Assistant.estimateTimeForBreakfast()
     }
 }
